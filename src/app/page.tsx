fdd04105ece957d848e1804f1d0c5fd8601970c1
Mohsen Azimi <mohsen.azimi@airbnb.com>
2023-06-26 20:02:05 +0200

    Add docs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-import Image from "next/image";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Head>
+        <title>Simple Chat App</title>
+      </Head>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <h1>
           <span className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
