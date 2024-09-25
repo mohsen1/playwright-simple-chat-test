07f72de1b99006b06b1c3e4de59eb97373425959
Mohsen Azimi <mohsen.azimi@airbnb.com>
2024-09-25 06:28:25 -0400

    Add collector

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "src/e2e",
   testMatch: "**/*.test.ts",
+  reporter: [["line"], ["buildkite-test-collector/playwright/reporter"]],
   webServer: {
     command: "yarn run start",
     port: 3000,
